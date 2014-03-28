7af1aa7d264fb6c9855872863a5641dc6dc0cc5f
PEMapModder <pemapmodder1970@gmail.com>
2014-03-28 19:33:53 +0800

    Indents and syntax errors
    @shoghicp said:
    > Always indent well.
    
    Indent well and then you will find the obvious errors like the curly braces.
    
    Also, sorry if I am hurting you, but well,
    
    > Learn PHP.

--- pvpmanagerplus.php
+++ pvpmanagerplus.php
@@ -10,38 +10,31 @@ apiversion=11,12,13
 */
 
 class PvPMang implements Plugin{
-private $api;
-public function __construct(ServerAPI $api, $server = false){
-$this->api = $api;
-}
-public function init(){
-        $this->api->addHandler("player.interact", array($this, "EventHandler"));
-                }
-    public function __destruct() {}
-
-    public function EventHandler($data, $event)
-    {
-  
-             case "player.interact":
-        $this->TEMP[$data["player"]->username] = true;
-                    if(isset($this->TEMP[$data["entity"]->player->username]) and $this->TEMP[$data["entity"]->player->username] == true)
-                    {                 
-                            $players = $this->api->player->online();
-                            for($i=0;$i<count($players);$i++) {
-                            $player = $this->api->player->get($players[$i]);
-                            if ($player->entity->getHealth() != 2) {
-                            $this->api->console->run("clearinventory " . $data["targetentity"]->player->username);
-                            $this->api->console->run("sudo " . $data["targetentity"]->player->username . " spawn");
-
-                        $this->api->chat->broadcast("" . $data["targetentity"]->player->username . " was killed by " . $data["entity"]->player->username);
-         }
-              }
-
-                      
-                        break;
+        private $api;
+        public function __construct(ServerAPI $api, $server = false){
+                $this->api = $api;
         }
-  
-break;
+        public function init(){
+                $this->api->addHandler("player.interact", array($this, "EventHandler"));
+        }
+        public function __destruct(){
+        }
+        public function EventHandler($data, $event){
+                switch($event){
+                case "player.interact":
+                        $this->TEMP[$data["player"]->username] = true;
+                        if(isset($this->TEMP[$data["entity"]->player->username]) and $this->TEMP[$data["entity"]->player->username] == true){                 
+                                $players = $this->api->player->online();
+                                for($i=0;$i<count($players);$i++){
+                                        $player = $this->api->player->get($players[$i]);
+                                        if($player->entity->getHealth() != 2){
+                                                $this->api->console->run("clearinventory " . $data["targetentity"]->player->username);
+                                                $this->api->console->run("sudo " . $data["targetentity"]->player->username . " spawn");
+                                                $this->api->chat->broadcast("" . $data["targetentity"]->player->username . " was killed by " . $data["entity"]->player->username);
+                                        }
+                                }
+                        }
+                        break;
+                }
+        }
 }
-}
-?>
